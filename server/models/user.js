096a1e646c7493c9be9b67301ea7c3581d2ecac3
eye1985 <eye1985@gmail.com>
2018-04-09 23:19:26 +0200

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,64 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-let User = mongoose.model('User', {
+let UserSchema = new mongoose.Schema({
     email : {
         type : String,
         require : true,
         trim: true,
-        minlength: 1
-    }
+        minlength: 1,
+        unique : true,
+        validate : {
+            validator : validator.isEmail,
+            message : '{VALUE} is not a valid email'
+        },
+    },
+    password:{
+        type: String,
+        require:true,
+        minlength: 6,
+    },
+    tokens: [{
+        access : {
+            type : String,
+            require : true
+        },
+
+        token : {
+            type : String,
+            require: true
+        }
+    }]
 });
+
+UserSchema.methods.toJSON = function(){
+    const user = this;
+    const userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function(){
+    const user = this;
+    const access = 'auth';
+    const token = jwt.sign({
+        _id: user._id.toHexString(),
+        access
+    }, 'secret').toString();
+
+    user.tokens = user.tokens.concat([{
+        access,
+        token
+    }]);
+
+    return user.save().then(() => {
+       return token;
+    });
+};
+
+const User = mongoose.model('User', UserSchema);
 
 module.exports = {
     User
--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-    let todo = new Todo({
+    const todo = new Todo({
         text: req.body.text
     });
 
@@ -108,6 +108,26 @@ app.patch('/todos/:id', (req, res) => {
     }).catch(error => {
         res.status(400).send();
     });
+});
+
+
+app.post('/users', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
+    const user = new User(body);
+
+    user.save()
+        .then(() => {
+            return user.generateAuthToken();
+        })
+        .then(token => {
+            res.header(
+                'x-auth',
+                token)
+                .send(user);
+        })
+        .catch(error => {
+            res.status(400).send(error);
+        });
 });
 
 app.listen(port, () => {
